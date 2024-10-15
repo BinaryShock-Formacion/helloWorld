3a3407dfb4c8fe3c464f025a06f481965cfa1178
Pablo <raistlin_majere_11@hotmail.com>
2024-10-15 13:52:05 +0200

    Tercera parte - parametro
    Tercera parte, permitir recibir un parámetro con el nombre

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Mensaje msg = new Mensaje();
-        System.out.println(msg.getMsg());
+        if(args.length > 0)
+        {
+            String nombre = args[0];
+            Mensaje mensaje = new Mensaje(nombre);
+            System.out.println(mensaje.getMsg());
+        }
+        else
+        {
+            Mensaje msg = new Mensaje();
+            System.out.println(msg.getMsg());
+        }
     }
 }

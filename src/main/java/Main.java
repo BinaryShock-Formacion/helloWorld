c2a50423584a5c07401ba1b12859bbaca2d4b73c
Pablo <raistlin_majere_11@hotmail.com>
2024-10-15 14:06:40 +0200

    Cuarta parte - jar Maven
    Cuarta parte, que se pueda ejecutar en consola desde un jar usando maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // La estructura de carpetas para un proyecto Maven debería ser la siguiente: src / main / java
+        // Hay que añadir plugin maven en el pom
+        // Para compilar: mvn clean package
+        // Para ejecutar: java -jar target/HelloWorld-1.0-SNAPSHOT.jar [argumentos]
         if(args.length > 0)
         {
             String nombre = args[0];

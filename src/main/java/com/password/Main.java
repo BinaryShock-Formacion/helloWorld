51574cf7c39c79409edcb892b0afb0c54e87142c
Pablo <raistlin_majere_11@hotmail.com>
2024-10-17 22:07:32 +0200

    Correcciones Aitor

--- src/main/java/com/password/Main.java
+++ src/main/java/com/password/Main.java
@@ -2,14 +2,6 @@ package com.password;
 
 public class Main {
     public static void main(String[] args) {
-        // La estructura de carpetas para un proyecto Maven debería ser la siguiente: src / main / java
-        // Hay que añadir plugin maven en el pom
-        // Para compilar: mvn clean package
-        // Para ejecutar: java -jar target/HelloWorld-1.0-SNAPSHOT.jar [argumentos]
-
-        // System.out.println(args.length > 0 ? "Hello " + args[0] : "Hello World!");
-        //System.out.println(new Mensaje(args.length > 0 ? args[0] : "").getMsg());
-        Mensaje mensaje = new Mensaje();
-        mensaje.imprimirMensaje(args);
+        Mensaje.imprimirMensaje(args);
     }
 }
--- src/main/java/com/password/Mensaje.java
+++ src/main/java/com/password/Mensaje.java
@@ -1,32 +1,13 @@
 package com.password;
 
 public class Mensaje {
-    private String msg;
 
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Mensaje() {
-        this.msg = "Hello world desde clase!";
-    }
-
-    public Mensaje(String nombre) {
-        this.msg = "Hello " + nombre +"!";
-    }
-
-    public void imprimirMensaje(String[] argumentos) {
-        /*if (argumentos.length > 0) {
-            System.out.println("Hello " + argumentos[0] + "!");
+    public static void imprimirMensaje(String[] argumentos) {
+        if (argumentos.length > 0) {
+            System.out.printf("Hello " + argumentos[0] + "!");
         }
         else {
-            System.out.println(this.msg);
-        }*/
-
-        System.out.println(argumentos.length > 0 ? "Hello " + argumentos[0] + "!" : this.msg);
+            System.out.printf("Hello world desde clase!");
+        }
     }
 }
